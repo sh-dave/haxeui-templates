d9c48539b979117d2fe3b8207b06fe31eff348ea
David Klein <dave@smokinheads.de>
2019-08-07 23:38:52 +0200

    use latest api calls

--- kha/skeleton/src/Main.hx
+++ kha/skeleton/src/Main.hx
@@ -1,21 +1,29 @@
 package;
 
-import kha.System;
-
 class Main {
     public static function main() {
-        #if (kha_version >= 1607)
+        kha.System.start({ title: "HaxeUIApp", width: 800, height: 600 }, function ( _ ) {
+            kha.Assets.loadEverything(function() {
+                haxe.ui.Toolkit.init();
 
-        System.init({title: "HaxeUIApp", width: 800, height: 600}, function () {
-            new KhaApplication();
+                final screen = haxe.ui.core.Screen.instance;
+                final ui = haxe.ui.macros.ComponentMacros.buildComponent("../assets/ui/main.xml");
+
+                screen.addComponent(ui);
+
+                var testButton2 = ui.findComponent("testButton2", haxe.ui.components.Button, true);
+                testButton2.onClick = function( _ ) {
+                    testButton2.text = "Set from code!";
+                }
+
+                kha.System.notifyOnFrames(function( framebuffers: Array<kha.Framebuffer> ) {
+                    final fb = framebuffers[0];
+                    final g2 = fb.g2;
+                    g2.begin(true, kha.Color.White);
+                        screen.renderTo(g2);
+                    g2.end();
+                });
+            });
         });
-
-        #else
-
-        System.init("HaxeUIApp", 800, 600, function () {
-            new KhaApplication();
-        });
-
-        #end
     }
 }
